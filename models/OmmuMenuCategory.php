a524c58643c3fd37bf5bb99af2271aeaec785b1c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-20 15:54:09 +0700

    fixed regenerate menu category model

--- models/OmmuMenuCategory.php
+++ models/OmmuMenuCategory.php
@@ -6,18 +6,8 @@
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co) 
  * @created date 15 January 2016, 16:53 WIB
+ * @modified date 20 January 2018, 06:28 WIB
  * @link https://github.com/ommu/ommu-core
- *
- * This is the template for generating the model class of a specified table.
- * - $this: the ModelCode object
- * - $tableName: the table name for this class (prefix is already removed if necessary)
- * - $modelClass: the model class name
- * - $columns: list of table columns (name=>CDbColumnSchema)
- * - $labels: list of attribute labels (name=>label)
- * - $rules: list of validation rules
- * - $relations: list of relations (name=>relation declaration)
- *
- * --------------------------------------------------------------------------------------
  *
  * This is the model class for table "ommu_core_menu_category".
  *
@@ -32,16 +22,20 @@
  * @property string $modified_date
  * @property string $modified_id
  * @property string $updated_date
+ * @property string $slug
  *
  * The followings are the available model relations:
- * @property CoreMenu[] $CoreMenus
+ * @property OmmuMenus[] $menus
+ * @property Users $creation;
+ * @property Users $modified;
  */
-class OmmuMenuCategory extends CActiveRecord
+
+class OmmuMenuCategory extends OActiveRecord
 {
-	public $defaultColumns = array();
+	public $gridForbiddenColumn = array('desc_i','modified_date','modified_search','updated_date','slug');
 	public $name_i;
 	public $desc_i;
-	
+
 	// Variable Search
 	public $creation_search;
 	public $modified_search;
@@ -90,19 +84,15 @@ class OmmuMenuCategory extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('publish,
-				name_i', 'required'),
+			array('publish, name_i', 'required'),
 			array('publish, creation_id, modified_id', 'numerical', 'integerOnly'=>true),
 			array('name, desc, creation_id, modified_id', 'length', 'max'=>11),
-			array('cat_code,
-				name_i', 'length', 'max'=>32),
-			array('
-				desc_i', 'length', 'max'=>128),
-			array('cat_code, 
-				desc_i', 'safe'),
+			array('cat_code, slug, name_i', 'length', 'max'=>32),
+			array('desc_i', 'length', 'max'=>128),
+			array('cat_code, desc_i', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('cat_id, publish, name, desc, cat_code, creation_date, creation_id, modified_date, modified_id, updated_date,
+			array('cat_id, publish, name, desc, cat_code, creation_date, creation_id, modified_date, modified_id, updated_date, slug,
 				name_i, desc_i, creation_search, modified_search, menu_search', 'safe', 'on'=>'search'),
 		);
 	}
@@ -116,11 +106,11 @@ class OmmuMenuCategory extends CActiveRecord
 		// class name for the relations automatically generated below.
 		return array(
 			'view' => array(self::BELONGS_TO, 'ViewMenuCategory', 'cat_id'),
+			'menus' => array(self::HAS_MANY, 'OmmuMenu', 'cat_id'),
 			'title' => array(self::BELONGS_TO, 'SourceMessage', 'name'),
 			'description' => array(self::BELONGS_TO, 'SourceMessage', 'desc'),
 			'creation' => array(self::BELONGS_TO, 'Users', 'creation_id'),
 			'modified' => array(self::BELONGS_TO, 'Users', 'modified_id'),
-			'menus' => array(self::HAS_MANY, 'OmmuMenu', 'cat_id'),
 		);
 	}
 
@@ -140,6 +130,7 @@ class OmmuMenuCategory extends CActiveRecord
 			'modified_date' => Yii::t('attribute', 'Modified Date'),
 			'modified_id' => Yii::t('attribute', 'Modified'),
 			'updated_date' => Yii::t('attribute', 'Updated Date'),
+			'slug' => Yii::t('attribute', 'Slug'),
 			'name_i' => Yii::t('attribute', 'Category'),
 			'desc_i' => Yii::t('attribute', 'Description'),
 			'creation_search' => Yii::t('attribute', 'Creation'),
@@ -165,7 +156,7 @@ class OmmuMenuCategory extends CActiveRecord
 		// @todo Please modify the following code to remove attributes that should not be searched.
 
 		$criteria=new CDbCriteria;
-		
+
 		// Custom Search
 		$criteria->with = array(
 			'view' => array(
@@ -181,128 +172,105 @@ class OmmuMenuCategory extends CActiveRecord
 			),
 			'creation' => array(
 				'alias'=>'creation',
-				'select'=>'displayname'
+				'select'=>'displayname',
 			),
 			'modified' => array(
 				'alias'=>'modified',
-				'select'=>'displayname'
+				'select'=>'displayname',
 			),
 		);
 
-		$criteria->compare('t.cat_id',$this->cat_id);
-		if(isset($_GET['type']) && $_GET['type'] == 'publish')
-			$criteria->compare('t.publish',1);
-		elseif(isset($_GET['type']) && $_GET['type'] == 'unpublish')
-			$criteria->compare('t.publish',0);
-		elseif(isset($_GET['type']) && $_GET['type'] == 'trash')
-			$criteria->compare('t.publish',2);
+		$criteria->compare('t.cat_id', $this->cat_id);
+		if(Yii::app()->getRequest()->getParam('type') == 'publish')
+			$criteria->compare('t.publish', 1);
+		elseif(Yii::app()->getRequest()->getParam('type') == 'unpublish')
+			$criteria->compare('t.publish', 0);
+		elseif(Yii::app()->getRequest()->getParam('type') == 'trash')
+			$criteria->compare('t.publish', 2);
 		else {
-			$criteria->addInCondition('t.publish',array(0,1));
-			$criteria->compare('t.publish',$this->publish);
+			$criteria->addInCondition('t.publish', array(0,1));
+			$criteria->compare('t.publish', $this->publish);
 		}
 		$criteria->compare('t.name',$this->name);
 		$criteria->compare('t.desc',$this->desc);
-		$criteria->compare('t.cat_code',strtolower($this->cat_code),true);
-		if($this->creation_date != null && !in_array($this->creation_date, array('0000-00-00 00:00:00', '0000-00-00')))
-			$criteria->compare('date(t.creation_date)',date('Y-m-d', strtotime($this->creation_date)));
-		$criteria->compare('t.creation_id', isset($_GET['creation']) ? $_GET['creation'] : $this->creation_id);
-		if($this->modified_date != null && !in_array($this->modified_date, array('0000-00-00 00:00:00', '0000-00-00')))
-			$criteria->compare('date(t.modified_date)',date('Y-m-d', strtotime($this->modified_date)));
-		$criteria->compare('t.modified_id', isset($_GET['modified']) ? $_GET['modified'] : $this->modified_id);
-		if($this->updated_date != null && !in_array($this->updated_date, array('0000-00-00 00:00:00', '0000-00-00')))
-			$criteria->compare('date(t.updated_date)',date('Y-m-d', strtotime($this->updated_date)));
-		
+		$criteria->compare('t.cat_code', strtolower($this->cat_code), true);
+		if($this->creation_date != null && !in_array($this->creation_date, array('0000-00-00 00:00:00', '1970-01-01 00:00:00')))
+			$criteria->compare('date(t.creation_date)', date('Y-m-d', strtotime($this->creation_date)));
+		$criteria->compare('t.creation_id', Yii::app()->getRequest()->getParam('creation') ? Yii::app()->getRequest()->getParam('creation') : $this->creation_id);
+		if($this->modified_date != null && !in_array($this->modified_date, array('0000-00-00 00:00:00', '1970-01-01 00:00:00')))
+			$criteria->compare('date(t.modified_date)', date('Y-m-d', strtotime($this->modified_date)));
+		$criteria->compare('t.modified_id', Yii::app()->getRequest()->getParam('modified') ? Yii::app()->getRequest()->getParam('modified') : $this->modified_id);
+		if($this->updated_date != null && !in_array($this->updated_date, array('0000-00-00 00:00:00', '1970-01-01 00:00:00')))
+			$criteria->compare('date(t.updated_date)', date('Y-m-d', strtotime($this->updated_date)));
+		$criteria->compare('t.slug', strtolower($this->slug), true);
+
 		$criteria->compare('title.message', strtolower($this->name_i), true);
 		$criteria->compare('description.message', strtolower($this->desc_i), true);
-		$criteria->compare('creation.displayname',strtolower($this->creation_search),true);
-		$criteria->compare('modified.displayname',strtolower($this->modified_search),true);
-		$criteria->compare('view.menus',$this->menu_search);
+		$criteria->compare('creation.displayname', strtolower($this->creation_search), true);
+		$criteria->compare('modified.displayname', strtolower($this->modified_search), true);
+		$criteria->compare('view.menus', $this->menu_search);
 
-		if(!isset($_GET['OmmuMenuCategory_sort']))
+		if(!Yii::app()->getRequest()->getParam('OmmuMenuCategory_sort'))
 			$criteria->order = 't.cat_id DESC';
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
 			'pagination'=>array(
-				'pageSize'=>30,
+				'pageSize'=>Yii::app()->params['grid-view'] ? Yii::app()->params['grid-view']['pageSize'] : 20,
 			),
 		));
-	}
-
-
-	/**
-	 * Get column for CGrid View
-	 */
-	public function getGridColumn($columns=null) {
-		if($columns !== null) {
-			foreach($columns as $val) {
-				/*
-				if(trim($val) == 'enabled') {
-					$this->defaultColumns[] = array(
-						'name'  => 'enabled',
-						'value' => '$data->enabled == 1? "Ya": "Tidak"',
-					);
-				}
-				*/
-				$this->defaultColumns[] = $val;
-			}
-		} else {
-			//$this->defaultColumns[] = 'cat_id';
-			$this->defaultColumns[] = 'publish';
-			$this->defaultColumns[] = 'name';
-			$this->defaultColumns[] = 'desc';
-			$this->defaultColumns[] = 'cat_code';
-			$this->defaultColumns[] = 'creation_date';
-			$this->defaultColumns[] = 'creation_id';
-			$this->defaultColumns[] = 'modified_date';
-			$this->defaultColumns[] = 'modified_id';
-			$this->defaultColumns[] = 'updated_date';
-		}
-
-		return $this->defaultColumns;
 	}
 
 	/**
 	 * Set default columns to display
 	 */
 	protected function afterConstruct() {
-		if(count($this->defaultColumns) == 0) {
-			/*
-			$this->defaultColumns[] = array(
+		if(count($this->templateColumns) == 0) {
+			$this->templateColumns['_option'] = array(
 				'class' => 'CCheckBoxColumn',
 				'name' => 'id',
 				'selectableRows' => 2,
 				'checkBoxHtmlOptions' => array('name' => 'trash_id[]')
 			);
-			*/
-			$this->defaultColumns[] = array(
-				'header' => 'No',
-				'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1'
+			$this->templateColumns['_no'] = array(
+				'header' => Yii::t('app', 'No'),
+				'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
 			);
-			$this->defaultColumns[] = array(
+			$this->templateColumns['cat_code'] = array(
 				'name' => 'cat_code',
 				'value' => '$data->cat_code',
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
 			);
-			$this->defaultColumns[] = array(
+			$this->templateColumns['name_i'] = array(
 				'name' => 'name_i',
 				'value' => '$data->title->message',
 			);
-			/*
-			$this->defaultColumns[] = array(
+			$this->templateColumns['desc_i'] = array(
 				'name' => 'desc_i',
 				'value' => '$data->description->message',
 			);
-			*/
-			$this->defaultColumns[] = array(
-				'name' => 'creation_search',
-				'value' => '$data->creation->displayname',
+			$this->templateColumns['menu_search'] = array(
+				'name' => 'menu_search',
+				'value' => 'CHtml::link($data->view->menus ? $data->view->menus : 0, Yii::app()->controller->createUrl(\'menu/manage\',array(\'category\'=>$data->cat_id)))',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
+				'type' => 'raw',
 			);
-			$this->defaultColumns[] = array(
+			if(!Yii::app()->getRequest()->getParam('creation')) {
+				$this->templateColumns['creation_search'] = array(
+					'name' => 'creation_search',
+					'value' => '$data->creation->displayname ? $data->creation->displayname : \'-\'',
+				);
+			}
+			$this->templateColumns['creation_date'] = array(
 				'name' => 'creation_date',
-				'value' => 'Utility::dateFormat($data->creation_date)',
+				'value' => '!in_array($data->creation_date, array(\'0000-00-00 00:00:00\', \'1970-01-01 00:00:00\')) ? Utility::dateFormat($data->creation_date) : \'-\'',
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
@@ -326,18 +294,72 @@ class OmmuMenuCategory extends CActiveRecord
 					),
 				), true),
 			);
-			$this->defaultColumns[] = array(
-				'name' => 'menu_search',
-				'value' => 'CHtml::link($data->view->menus ? $data->view->menus : 0, Yii::app()->controller->createUrl("menu/manage",array("category"=>$data->cat_id)))',
+			$this->templateColumns['modified_date'] = array(
+				'name' => 'modified_date',
+				'value' => '!in_array($data->modified_date, array(\'0000-00-00 00:00:00\', \'1970-01-01 00:00:00\')) ? Utility::dateFormat($data->modified_date) : \'-\'',
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'type' => 'raw',
+				'filter' => Yii::app()->controller->widget('application.libraries.core.components.system.CJuiDatePicker', array(
+					'model'=>$this,
+					'attribute'=>'modified_date',
+					'language' => 'en',
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
+					//'mode'=>'datetime',
+					'htmlOptions' => array(
+						'id' => 'modified_date_filter',
+					),
+					'options'=>array(
+						'showOn' => 'focus',
+						'dateFormat' => 'dd-mm-yy',
+						'showOtherMonths' => true,
+						'selectOtherMonths' => true,
+						'changeMonth' => true,
+						'changeYear' => true,
+						'showButtonPanel' => true,
+					),
+				), true),
 			);
-			if(!isset($_GET['type'])) {
-				$this->defaultColumns[] = array(
+			if(!Yii::app()->getRequest()->getParam('modified')) {
+				$this->templateColumns['modified_search'] = array(
+					'name' => 'modified_search',
+					'value' => '$data->modified->displayname ? $data->modified->displayname : \'-\'',
+				);
+			}
+			$this->templateColumns['updated_date'] = array(
+				'name' => 'updated_date',
+				'value' => '!in_array($data->updated_date, array(\'0000-00-00 00:00:00\', \'1970-01-01 00:00:00\')) ? Utility::dateFormat($data->updated_date) : \'-\'',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
+				'filter' => Yii::app()->controller->widget('application.libraries.core.components.system.CJuiDatePicker', array(
+					'model'=>$this,
+					'attribute'=>'updated_date',
+					'language' => 'en',
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
+					//'mode'=>'datetime',
+					'htmlOptions' => array(
+						'id' => 'updated_date_filter',
+					),
+					'options'=>array(
+						'showOn' => 'focus',
+						'dateFormat' => 'dd-mm-yy',
+						'showOtherMonths' => true,
+						'selectOtherMonths' => true,
+						'changeMonth' => true,
+						'changeYear' => true,
+						'showButtonPanel' => true,
+					),
+				), true),
+			);
+			$this->templateColumns['slug'] = array(
+				'name' => 'slug',
+				'value' => '$data->slug',
+			);
+			if(!Yii::app()->getRequest()->getParam('type')) {
+				$this->templateColumns['publish'] = array(
 					'name' => 'publish',
-					'value' => 'Utility::getPublish(Yii::app()->controller->createUrl("publish",array("id"=>$data->cat_id)), $data->publish, 1)',
+					'value' => 'Utility::getPublish(Yii::app()->controller->createUrl(\'publish\',array(\'id\'=>$data->cat_id)), $data->publish)',
 					'htmlOptions' => array(
 						'class' => 'center',
 					),
@@ -365,21 +387,21 @@ class OmmuMenuCategory extends CActiveRecord
 			
 		} else {
 			$model = self::model()->findByPk($id);
-			return $model;			
+			return $model;
 		}
 	}
 
 	/**
-	 * Get category
+	 * getCategory
 	 * 0 = unpublish
 	 * 1 = publish
 	 */
 	public static function getCategory($publish=null, $type=null) 
-	{		
+	{
 		$criteria=new CDbCriteria;
 		if($publish != null)
-			$criteria->compare('t.publish',$publish);
-		
+			$criteria->compare('t.publish', $publish);
+
 		$model = self::model()->findAll($criteria);
 
 		if($type == null) {
@@ -389,9 +411,8 @@ class OmmuMenuCategory extends CActiveRecord
 					$items[$val->cat_id] = $val->title->message;
 				}
 				return $items;
-			} else {
+			} else
 				return false;
-			}
 		} else
 			return $model;
 	}
@@ -410,12 +431,13 @@ class OmmuMenuCategory extends CActiveRecord
 	/**
 	 * before validate attributes
 	 */
-	protected function beforeValidate() {
+	protected function beforeValidate() 
+	{
 		if(parent::beforeValidate()) {
 			if($this->isNewRecord)
-				$this->creation_id = Yii::app()->user->id;	
+				$this->creation_id = !Yii::app()->user->isGuest ? Yii::app()->user->id : 0;
 			else
-				$this->modified_id = Yii::app()->user->id;
+				$this->modified_id = !Yii::app()->user->isGuest ? Yii::app()->user->id : 0;
 		}
 		return true;
 	}
@@ -437,7 +459,7 @@ class OmmuMenuCategory extends CActiveRecord
 				$name->location = $location.'_title';
 				if($name->save())
 					$this->name = $name->id;
-				
+
 				$this->slug = Utility::getUrlTitle($this->name_i);
 				
 			} else {
@@ -445,11 +467,11 @@ class OmmuMenuCategory extends CActiveRecord
 				$name->message = $this->name_i;
 				$name->save();
 			}
-			
+
 			if($this->isNewRecord || (!$this->isNewRecord && !$this->desc)) {
 				$desc=new SourceMessage;
 				$desc->message = $this->desc_i;
-				$desc->location = $location.'_description';
+				$desc->location = $location.'_desc';
 				if($desc->save())
 					$this->desc = $desc->id;
 				
@@ -458,7 +480,7 @@ class OmmuMenuCategory extends CActiveRecord
 				$desc->message = $this->desc_i;
 				$desc->save();
 			}
-			
+
 			if($action != 'publish')
 				$this->cat_code = Utility::getUrlTitle(strtolower(trim($this->name_i)));
 		}
