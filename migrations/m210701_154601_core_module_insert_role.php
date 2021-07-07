ce895d551ce8fc0aa307f071ce62dab4607e56f6
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2021-07-07 21:03:49 +0700

    fixed dynamic migrate

--- migrations/m210701_154601_core_module_insert_role.php
+++ migrations/m210701_154601_core_module_insert_role.php
@@ -11,22 +11,42 @@
  */
 
 use Yii;
+use yii\base\InvalidConfigException;
+use yii\rbac\DbManager;
 
 class m210701_154601_core_module_insert_role extends \yii\db\Migration
 {
+    /**
+     * @throws yii\base\InvalidConfigException
+     * @return DbManager
+     */
+    protected function getAuthManager()
+    {
+        $authManager = Yii::$app->getAuthManager();
+        if (!$authManager instanceof DbManager) {
+            throw new InvalidConfigException('You should configure "authManager" component to use database before executing this migration.');
+        }
+
+        return $authManager;
+    }
+
 	public function up()
 	{
-		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_auth_item';
+        $authManager = $this->getAuthManager();
+        $this->db = $authManager->db;
+        $schema = $this->db->getSchema()->defaultSchema;
+
+		$tableName = Yii::$app->db->tablePrefix . $authManager->itemTable;
 		if (Yii::$app->db->getTableSchema($tableName, true)) {
-			$this->batchInsert('ommu_core_auth_item', ['name', 'type', 'data', 'created_at'], [
+			$this->batchInsert($tableName, ['name', 'type', 'data', 'created_at'], [
 				['/admin/sync/tag/index', '2', '', time()],
 				['/admin/sync/tag/slug-to-camelize', '2', '', time()],
 			]);
 		}
 
-		$tableName = Yii::$app->db->tablePrefix . 'ommu_core_auth_item_child';
+		$tableName = Yii::$app->db->tablePrefix . $authManager->itemChildTable;
 		if (Yii::$app->db->getTableSchema($tableName, true)) {
-			$this->batchInsert('ommu_core_auth_item_child', ['parent', 'child'], [
+			$this->batchInsert($tableName, ['parent', 'child'], [
 				['userModerator', '/admin/sync/tag/index'],
 				['userModerator', '/admin/sync/tag/slug-to-camelize'],
 			]);
